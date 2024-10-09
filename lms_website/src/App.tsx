967f31903d7f315c3ef0b97ee788353b1c780b9c
hunny09 <137426009+hunny09@users.noreply.github.com>
2024-10-09 11:12:51 +0530

    Added Component

---
+++ lms_website/src/App.tsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+import HomepageBanner1 from "./Components/Frontend/HomePage/HomepageBanner1"
+import Banner2 from "./Components/Frontend/HomePage/HomepageBanner2"
+
+const App: React.FC = () => {
+  return (
+
+    <div >
+      <HomepageBanner1/>
+      
+      <Banner2/>
+  </div>
+  )
+}
+
+export default App
