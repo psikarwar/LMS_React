1e18f9ebb243cb9f96395621b4f38c6eceee0b10
hunny09 <137426009+hunny09@users.noreply.github.com>
2024-10-09 13:48:14 +0530

    deleted all files

--- lms_website/src/App.tsx
+++ lms_website/src/App.tsx
@@ -1,15 +1,12 @@
 // import React from 'react'
 
-import HomepageBanner1 from "./Components/Frontend/HomePage/HomepageBanner1"
-import Banner2 from "./Components/Frontend/HomePage/HomepageBanner2"
+
 
 const App: React.FC = () => {
   return (
 
     <div >
-      <HomepageBanner1/>
-      
-      <Banner2/>
+    
   </div>
   )
 }
